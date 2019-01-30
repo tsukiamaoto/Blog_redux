d0bde2bfc383ca9e8660fa27238053d34f6e2a5a
Gary <gomrdhl796@gmail.com>
2019-01-30 18:29:36 +0800

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 //initial express server
 const app = express();
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '8080';
 //analyse html message e.g.POST,PUT,DELEDE
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
