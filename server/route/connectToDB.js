6154351f11bb5e3d67103b18f90f4545c3a88b73
Gary <gomrdhl796@gmail.com>
2019-01-30 12:59:38 +0800

    mongodb online link

--- server/route/connectToDB.js
+++ server/route/connectToDB.js
@@ -2,8 +2,7 @@
 const mongoose = require('mongoose');
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1:27017/myapp';
-mongoose.connect(mongoDB);
+var mongoDB = 'mongodb://gomrdhl796@gmail.com:!easyDB66@ds021915.mlab.com:21915/blog-9527'
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 console.log('Database connection');
