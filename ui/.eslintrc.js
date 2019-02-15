91323281c7f01ed1cbd8ef6445728d0496bd9a82
Gary <gomrdhl796@gmail.com>
2019-02-15 17:56:27 +0800

    １

--- ui/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'extends': ['standard', 'plugin:react'],
-  'react/prop-types': [
-    'enabled',
-    { ignore: 'ignore', customValidators: 'customValidator' }
-  ],
-  'globals': { fetch: false }
-}
